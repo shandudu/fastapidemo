043733df8b70aeeb8314135340b873a33b505cd8
xxx <shandudu233@gmail.com>
2024-06-12 22:40:56 +0800

    111

---
+++ PlaywrighrtDemo/tests/test_my_search_baidu.py
@@ -0,0 +1,8 @@
+from playwright.sync_api import Page, expect
+
+def test_baidu(page: Page):
+    page.goto(url="https://www.baidu.com")
+    page.wait_for_timeout(2_000)
+    page.locator('//input[@name="wd"]').fill("playwright")
+    page.get_by_text("百度一下").click()
+    expect(page.get_by_text("https://github.com/microsoft/playwright")).to_be_visible()
