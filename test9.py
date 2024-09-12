19923ae245e9914f2dfa1c5a31578f5d07ab035a
xxx <shandudu233@gmail.com>
2024-09-12 17:27:45 +0800

    Default Changelist4

--- test9.py
+++ test9.py
@@ -1,6 +1,8 @@
-from typing import Annotated
+from typing import Annotated, Union
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -14,19 +16,77 @@ data = {
 class OwnerError(Exception):
     pass
 
+class InternalError(Exception):
+    pass
+
+# def get_username():
+#     try:
+#         yield "Rick"
+#     except OwnerError as e:
+#         raise HTTPException(status_code=400, detail=f"Owner error: {e}")
+#
+#
+# @app.get("/items/{item_id}")
+# def get_item(item_id: str, username: Annotated[str, Depends(get_username)]):
+#     if item_id not in data:
+#         raise HTTPException(status_code=404, detail="Item not found")
+#     item = data[item_id]
+#     if item["owner"] != username:
+#         raise OwnerError(username)
+#     return item
 
 def get_username():
     try:
         yield "Rick"
-    except OwnerError as e:
-        raise HTTPException(status_code=400, detail=f"Owner error: {e}")
+    except InternalError:
+        print("We don't swallow the internal error here, we raise again 😎")
+        raise
 
 
 @app.get("/items/{item_id}")
 def get_item(item_id: str, username: Annotated[str, Depends(get_username)]):
-    if item_id not in data:
-        raise HTTPException(status_code=404, detail="Item not found")
-    item = data[item_id]
-    if item["owner"] != username:
-        raise OwnerError(username)
-    return item
+    if item_id == "portal-gun":
+        raise InternalError(
+            f"The portal gun is too dangerous to be owned by {username}"
+        )
+    if item_id != "plumbus":
+        raise HTTPException(
+            status_code=404, detail="Item not found, there's only a plumbus here"
+        )
+    return item_id
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
+
+
+
+
+
+
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
+    )
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    return user
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
